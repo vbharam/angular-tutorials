d5f559224a1b44727d2ccbd7f2e80d57f8a43449
Vishal Bharam <vbharam@gmail.com>
2016-07-31 17:02:43 -0700

    Initial Commit

---
+++ Top10App/js/controllers/MainController.js
@@ -0,0 +1,9 @@
+app.controller('MainController', ['$scope','shows', function($scope, shows) {
+  
+  shows.success(function(data) {
+    $scope.shows = data;
+  });
+  
+  $scope.name = "hi";
+  
+}]);
